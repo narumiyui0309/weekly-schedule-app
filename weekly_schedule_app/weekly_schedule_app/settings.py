3f9ba5a0377b576081dbc2e5cb070b7d9bd84295
narumiyui0921 <narumiyui0921@gmail.com>
2023-05-16 18:25:32 +0900

    DBをmysqlに変更

--- weekly_schedule_app/weekly_schedule_app/settings.py
+++ weekly_schedule_app/weekly_schedule_app/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'weekly_schedule_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'weeklyScheduleApp',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
